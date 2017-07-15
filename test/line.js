ca42e154f660e612ced9f5116ca0d20a14e36b44
ajlopez <webmaster@ajlopez.com>
2017-07-15 17:48:48 -0300

    Render simple line

--- test/line.js
+++ test/line.js
@@ -1,10 +1,21 @@
 
 var sd = require('..');
+var domie = require('domie');
 
 exports['create simple line'] = function (test) {
 	var line = sd.line({ x1: 10, y1: 20, x2: 30, y2: 40 });
 	
 	test.ok(line);
 	test.equal(typeof line, 'object');
+};
+
+exports['render simple line element'] = function (test) {
+	var line = sd.line({ x1: 10, y1: 20, x2: 30, y2: 40 });
+	var document = domie.document();
+	
+	var element = line.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" />');
 };
 
