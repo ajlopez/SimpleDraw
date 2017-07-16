81c636e83b74d174a963818fc02b0a7f408f98a6
ajlopez <webmaster@ajlopez.com>
2017-07-16 10:46:58 -0300

    Circle first implementation and tests

---
+++ test/circle.js
@@ -0,0 +1,31 @@
+
+var sd = require('..');
+var domie = require('domie');
+
+exports['create simple circle'] = function (test) {
+	var circle = sd.circle({ cx: 10, cy: 10, r: 5 });
+	
+	test.ok(circle);
+	test.equal(typeof circle, 'object');
+};
+
+exports['render simple circle element'] = function (test) {
+	var circle = sd.circle({ cx: 10, cy: 20, r: 5 });
+	var document = domie.document();
+	
+	var element = circle.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<circle cx="10" cy="20" r="5" />');
+};
+
+exports['use stroke in options'] = function (test) {
+	var circle = sd.circle({ cx: 10, cy: 20, r: 5, stroke: 'black' });
+	var document = domie.document();
+	
+	var element = circle.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<circle cx="10" cy="20" r="5" style="stroke: black;" />');
+};
+
