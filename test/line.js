8784835015926fc7d568c135fc0bd89da7248429
ajlopez <webmaster@ajlopez.com>
2017-07-16 10:39:16 -0300

    Stroke in line style

--- test/line.js
+++ test/line.js
@@ -19,3 +19,13 @@ exports['render simple line element'] = function (test) {
 	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" />');
 };
 
+exports['use stroke in options'] = function (test) {
+	var line = sd.line({ x1: 10, y1: 20, x2: 30, y2: 40, stroke: "black" });
+	var document = domie.document();
+	
+	var element = line.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" style="stroke: black;" />');
+};
+
