75b856f323c4da36ce07dfa9e37e855db08c24f5
ajlopez <webmaster@ajlopez.com>
2017-07-22 16:59:18 -0300

    Fill opacity

--- test/rectangle.js
+++ test/rectangle.js
@@ -19,3 +19,12 @@ exports['render simple rectangle element'] = function (test) {
 	test.equal(element.outerHTML, '<rect x="10" y="20" width="50" height="25" />');
 };
 
+exports['render rectangle with fill and fill opacity'] = function (test) {
+	var rectangle = sd.rectangle({ x: 10, y: 20, width: 50, height: 25, fill: 'blue', fill_opacity: 0.2 });
+	var document = domie.document();
+	
+	var element = rectangle.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<rect x="10" y="20" width="50" height="25" style="fill: blue; fill-opacity: 0.2;" />');
+};
