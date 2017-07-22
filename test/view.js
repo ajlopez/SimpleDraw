eac2bccff427c058e867d1ad763bf4f9b5809097
ajlopez <webmaster@ajlopez.com>
2017-07-22 16:38:24 -0300

    Render rectangle in view

--- test/view.js
+++ test/view.js
@@ -69,3 +69,16 @@ exports['render view with circle and line'] = function (test) {
 	test.equal(element.outerHTML, '<svg width="100px" height="50px"><circle cx="10" cy="20" r="5" /><line x1="10" y1="20" x2="30" y2="40" /></svg>');
 };
 
+exports['render view with line and rectangle'] = function (test) {
+	var view = sd.view({ width: 100, height: 50 })
+		.line({ x1: 10, y1: 20, x2: 30, y2: 40 })
+		.rectangle({ x: 10, y: 20, width: 50, height: 25 });
+		
+	var document = domie.document();
+	
+	var element = view.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<svg width="100px" height="50px"><line x1="10" y1="20" x2="30" y2="40" /><rect x="10" y="20" width="50" height="25" /></svg>');
+};
+
