5e490d6160e8208f0b2dc39074c0e122b6c00754
ajlopez <webmaster@ajlopez.com>
2017-07-22 17:01:09 -0300

    Rounded corners in rectangle

--- test/rectangle.js
+++ test/rectangle.js
@@ -19,6 +19,16 @@ exports['render simple rectangle element'] = function (test) {
 	test.equal(element.outerHTML, '<rect x="10" y="20" width="50" height="25" />');
 };
 
+exports['render rectangle with rounded corners'] = function (test) {
+	var rectangle = sd.rectangle({ x: 10, y: 20, width: 50, height: 25, rx: 3, ry: 2 });
+	var document = domie.document();
+	
+	var element = rectangle.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<rect x="10" y="20" width="50" height="25" rx="3" ry="2" />');
+};
+
 exports['render rectangle with fill and fill opacity'] = function (test) {
 	var rectangle = sd.rectangle({ x: 10, y: 20, width: 50, height: 25, fill: 'blue', fill_opacity: 0.2 });
 	var document = domie.document();
