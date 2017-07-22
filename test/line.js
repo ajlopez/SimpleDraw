e7ccd649a61d2d618525a5dbd5856a64cfe7a61f
ajlopez <webmaster@ajlopez.com>
2017-07-22 16:18:28 -0300

    Stroke opacity

--- test/line.js
+++ test/line.js
@@ -39,3 +39,13 @@ exports['use stroke and stroke width in options'] = function (test) {
 	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" style="stroke: black; stroke-width: 10;" />');
 };
 
+exports['use stroke and stroke opacity in options'] = function (test) {
+	var line = sd.line({ x1: 10, y1: 20, x2: 30, y2: 40, stroke: "black", stroke_opacity: 0.2 });
+	var document = domie.document();
+	
+	var element = line.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" style="stroke: black; stroke-opacity: 0.2;" />');
+};
+
