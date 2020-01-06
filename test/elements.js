8a1527658523f2d7c1bead8798d863d6fe423364
ajlopez <webmaster@ajlopez.com>
2020-01-06 10:34:58 -0300

    Create rectangle element

--- test/elements.js
+++ test/elements.js
@@ -12,4 +12,12 @@ exports['create line element'] = function (test) {
 	test.equal(element.outerHTML, '<line x1="10" y1="20" x2="30" y2="40" />');
 };
 
-
+exports['create rectangle element'] = function (test) {
+	const rectangle = sd.element('rect', { x: 10, y: 20, width: 50, height: 25 });
+	const document = domie.document();
+	
+	const element = rectangle.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<rect x="10" y="20" width="50" height="25" />');
+};
