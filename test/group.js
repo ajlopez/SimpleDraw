a344af7321e23f882cba1ee4d6c78c6ee6086272
ajlopez <webmaster@ajlopez.com>
2020-01-22 14:46:52 -0300

    Add element to group

--- test/group.js
+++ test/group.js
@@ -30,3 +30,14 @@ exports['render group element with line element'] = function (test) {
 	test.equal(element.outerHTML, '<g id="g1"><line x1="10" y1="20" x2="30" y2="40" /></g>');
 };
 
+exports['render group element adding line element'] = function (test) {
+	const group = sd.group({ id: 'g1' });
+    group.add(sd.element('line', { x1: 10, y1: 20, x2: 30, y2: 40 }));
+	const document = domie.document();
+	
+	const element = group.element(document);
+	
+	test.ok(element);
+	test.equal(element.outerHTML, '<g id="g1"><line x1="10" y1="20" x2="30" y2="40" /></g>');
+};
+
